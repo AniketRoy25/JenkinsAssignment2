91588caae28e0fd402b28af30951da90cea2038b
Aniket <aniketroy97@outlook.com>
2024-01-20 07:14:51 +0000

    assignment 2

---
+++ JenkinsAssignment2/src/jenkinsAssignment2/Addition.java
@@ -0,0 +1,16 @@
+package jenkinsAssignment2;
+
+public class Addition {
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 5;
+		int c = a+b;
+		System.out.println("Addition is : " + c);
+	}
+
+}
